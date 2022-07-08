59baff023cb12bc55bccb3738f5c222e7d162872
Rustambek <rustambeksafarovv@gmail.com>
2022-07-08 09:32:40 +0500

    Redone

--- bin/while_loop10.dart
+++ bin/while_loop10.dart
@@ -6,4 +6,20 @@
   
   
 */
-void main() {}
+String func(a){
+  int b = 0;
+  int d = 0;
+  String x = "tub son";
+  String y = "tub son emas";
+  while (b < a){
+  
+  b ++;
+    if (a % b == 0){
+      return x;
+    }    
+  }
+  return y;
+}
+void main() {
+  print(func(8));
+}
